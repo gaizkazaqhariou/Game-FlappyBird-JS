c0644e1b5f0d9b2d65ec9b4bc84317ee5b42dfa3
gaizkazaqhariou <gaizkapolinema@gmail.com>
2020-03-15 19:34:10 +0700

    first commit

---
+++ sketch.js
@@ -0,0 +1,78 @@
+var bird;
+var pipes = [];
+
+function setup() {
+	createCanvas(400, 600);
+	bird = new Bird();
+	pipes.push(new Pipe());
+}
+
+function draw() {
+	background(0);
+
+	for(var i = pipes.length-1; i >= 0; i--){
+		pipes[i].show();
+		pipes[i].update();
+
+		if (pipes[i].hits(bird)) {
+
+		}
+
+		if (pipes[i].offscreen()) {
+			pipes.splice(i, 1);
+		}
+	}
+
+	bird.update();
+	bird.show();
+
+	if (frameCount % 100 == 0) {
+		pipes.push(new Pipe());
+	}
+}
+
+function keyPressed() {
+	if (key == ' ') {
+		bird.up();
+	}
+}
+
+// var bird;
+// var pipes = [];
+
+// function setup() {
+//   createCanvas(640, 480);
+//   bird = new Bird();
+//   pipes.push(new Pipe());
+// }
+
+// function draw() {
+//   background(0);
+
+//   for (var i = pipes.length-1; i >= 0; i--) {
+//     pipes[i].show();
+//     pipes[i].update();
+
+//     if (pipes[i].hits(bird)) {
+//       console.log("HIT");
+//     }
+
+//     if (pipes[i].offscreen()) {
+//       pipes.splice(i, 1);
+//     }
+//   }
+
+//   bird.update();
+//   bird.show();
+
+//   if (frameCount % 75 == 0) {
+//     pipes.push(new Pipe());
+//   }
+// }
+
+// function keyPressed() {
+//   if (key == ' ') {
+//     bird.up();
+//     //console.log("SPACE");
+//   }
+// }
